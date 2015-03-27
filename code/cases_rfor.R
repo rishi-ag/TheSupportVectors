847bdf37ec26694652b1cd05a9d1c15573502afe
j-verdu <januoc@gmail.com>
2015-03-27 15:07:40 +0100

    report and plots

--- code/cases_rfor.R
+++ code/cases_rfor.R
@@ -35,4 +35,30 @@ probs<-foreach(i = i,
                    predict(models[[i]], test.feat,type="prob")
                }
 
+### Finally we keep the second case to predict, and look at results
+setwd("D:/master/kaggle/TheSupportVectors")
+model<-readRDS("data/rforest_model2.rds")
 
+# Look at importance of features
+importance<-model$importance
+importance<-data.frame(feature=row.names(importance),importance=as.vector(importance))
+importance$type<-c(rep("topo",2),rep("water",2),"antropic",rep("topo",3),"antropic",
+                   rep("area",4),rep("soil",40),rep("topo",2))
+importance2<-importance[order(importance$importance,decreasing=TRUE),]
+importance2$importance<-sqrt(importance2$importance)
+importance2 <- transform(importance2, 
+                          feature = reorder(feature, importance))
+plot<-ggplot(importance2, aes(x=feature, y=importance,fill=type)) +
+    geom_bar(stat='identity') + ylab("Importance (sqrt of mean decrease Gini index)")+
+    coord_flip()
+#save plot
+jpeg(filename = 'plots/importance_rfor.jpg', units = "in", width = 7, height = 7, res = 600)
+plot
+dev.off()
+
+# Check at accuracy
+confusion<-model$confusion
+table(confusion)
+heatmap(confusion[,-8])
+
+write.csv(confusion,"data/rfor_confusion.csv")
